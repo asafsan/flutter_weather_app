cfb2dbd4948b6779ecbc7587dc2b8c50bb60d2bb
asafsan <asaf.san@hotmail.com>
2023-01-13 19:14:08 +0300

    first commit

---
+++ lib/services/weather_api_client.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather_app/model/weather_model.dart';
+
+class WeatherApiClient {
+  Future<Weather>? getCurrentWeather(String? location) async {
+    var endpoint = Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=21758b7c2591384ee052fb0739ea5a2c&units=metric");
+
+    var response = await http.get(endpoint);
+    var body = jsonDecode(response.body);
+    print(Weather.fromJson(body));
+    return Weather.fromJson(body);
+  }
+}
